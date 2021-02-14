39f1eaa2c2b9eeaa231a321be8cb8099f1c6c734
Derek Morgan <derekm1215@gmail.com>
2021-02-14 02:05:39 +0000

    subnet refs

--- variables.tf
+++ variables.tf
@@ -7,7 +7,9 @@ variable "instance_type" {
   default = "t2.micro"
 }
 variable "public_sg" {}
-variable "public_subnets" {}
+variable "public_subnets" {
+  default = module.networking.public_subnets
+}
 variable "vol_size" {
   default = 8
 }
