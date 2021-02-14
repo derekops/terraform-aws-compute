fb5ec942dcacbfa65f54f59ef8d2250549ea83f4
Derek Morgan <derekm1215@gmail.com>
2021-02-14 01:47:16 +0000

    initial

---
+++ variables.tf
@@ -0,0 +1,17 @@
+# --- compute/variables.tf ---
+
+variable "instance_count" {
+    default = 1
+}
+variable "instance_type" {
+    default = "t2.micro"
+}
+variable "public_sg" {}
+variable "public_subnets" {}
+variable "vol_size" {
+    default = 8
+}
+variable "public_key_material" {}
+variable "key_name" {
+    default = "mtckey"
+}
